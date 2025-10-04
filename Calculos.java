aba427deae9440beb702a601f859765589ed5678
Raíssa de Castro <RAISSACVF@OUTLOOK.COM>
2025-10-04 15:23:00 -0300

    Create Calculos.java

---
+++ Calculos.java
@@ -0,0 +1,19 @@
+public class Calculos {
+    public double calcular(double num1, double num2, String operador) {
+        switch (operador) {
+            case "+":
+                return num1 + num2;
+            case "-":
+                return num1 - num2;
+            case "*":
+                return num1 * num2;
+            case "/":
+                if (num2 == 0) {
+                    throw new IllegalArgumentException("Erro: Divisão por zero não é permitida.");
+                }
+                return num1 / num2;
+            default:
+                throw new IllegalArgumentException("Operador inválido. Use apenas +, -, *, ou /.");
+        }
+    }
+}
